15388fc18f9051f487d70c635e125c1e9960c3db
Kevin Phair <phair.kevin@gmail.com>
2019-09-23 17:54:57 -0400

    replace external dependency with encoding/csv, add some argument validation

--- main.go
+++ main.go
@@ -5,79 +5,136 @@ package main
 // or to be worse. This script can help us work that out. Right now, it only looks
 // at the ISO country code, but checking more fields should be easy enough.
 import (
+    "encoding/csv"
     "flag"
 	"fmt"
-	"github.com/oschwald/geoip2-golang"
-	"github.com/yunabe/easycsv"
+    "io"
 	"log"
 	"net"
+    "os"
+    "path/filepath"
 	"strings"
+
+	"github.com/oschwald/geoip2-golang"
 )
 
 // Usage Example:
 // go run go/check_csv_corrections/main.go /path/to/corrections.csv /path/to/mmdbfile.mmdb
 func main() {
-    csvFilename := flag.String(
-        "geofeed-path",
-        "",
-        "Path to the local geofeed file to verify",
-    )
-
-    mmdbFilename := flag.String(
-        "mmdb-path",
-        "/usr/local/share/GeoIP/GeoIP2-City.mmdb",
-        "Path to MMDB file to compare geofeed file against",
-    )
-
-    flag.Parse()
-
-    if ( *csvFilename == "" ) {
-        err := "'--geofeed-path' is required, and should be a path to a local geofeed file"
+    geofeedFilename, mmdbFilename, err := parseArgs()
+    if err != nil {
         log.Fatal(err)
     }
 
-	db, err := geoip2.Open(*mmdbFilename)
+	db, err := geoip2.Open(mmdbFilename)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
 
 	totalCount, correctionCount := 0, 0
-	opt := easycsv.Option{
-		Comment: '#',
-	}
-	r := easycsv.NewReaderFile(*csvFilename, opt)
-	err = r.Loop(func(entry *struct {
-		Subnet  string `index:"0"`
-		Country string `index:"1"`
-		Region  string `index:"2"`
-		City    string `index:"3"`
-		Postal  string `index:"4"`
-	}) error {
-		ip, _, err := net.ParseCIDR(entry.Subnet)
-		if err != nil {
-			log.Fatal(err)
-		}
-		totalCount++
-		record, err := db.City(ip)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if !(strings.EqualFold(entry.Country, record.Country.IsoCode)) || !(strings.EqualFold(entry.City, record.City.Names["en"])) {
-			correctionCount++
-			firstSubdivision := ""
-			for i, sd := range record.Subdivisions {
-				if i == 0 {
-					firstSubdivision = sd.IsoCode
-					break
-				}
-			}
-			fmt.Printf("Found a potential improvement: %v, current country: '%v', suggested country: '%v', current city: '%v', suggested city: '%v', current region: '%v', suggested region: '%v,'\n", entry.Subnet, record.Country.IsoCode, entry.Country, record.City.Names["en"], entry.City, firstSubdivision, entry.Region)
-		}
-		return nil
-	})
-	if err != nil {
+    geofeedFH, err := os.Open(geofeedFilename)
+    if err != nil {
+        log.Fatal(err)
+    }
+    csvReader := csv.NewReader(geofeedFH)
+    csvReader.ReuseRecord = true
+    csvReader.Comment = '#'
+    csvReader.FieldsPerRecord = 5
+    csvReader.TrimLeadingSpace = true
+    defer geofeedFH.Close()
+
+    for {
+        row, err := csvReader.Read()
+        if err == io.EOF {
+            break
+        }
+        if err != nil {
+            log.Fatal(err)
+        }
+        correctionCount += verifyCorrection(row, db)
+    }
+	if (err != nil && err != io.EOF) {
 		log.Fatalf("Failed to read file: %v", err)
 	}
-	fmt.Printf("\nOut of %v potential corrections, %v may be different than our current mappings\n", totalCount, correctionCount)
+	fmt.Printf(
+        "\nOut of %v potential corrections, %v may be different than our current mappings\n",
+        totalCount,
+        correctionCount,
+    )
+}
+
+func verifyCorrection(correction []string, db *geoip2.Reader ) (int) {
+    /*
+    0: network (CIDR or single IP)
+    1: ISO-3166 country code
+    2: ISO-3166-2 region code
+    3: city name
+    4: postal code
+    */
+    networkOrIP := correction[0]
+    if !(strings.Contains(networkOrIP, "/") ) {
+        if strings.Contains(networkOrIP, ":") {
+            networkOrIP += "/128"
+        } else {
+            networkOrIP += "/32"
+        }
+    }
+    network, _, err := net.ParseCIDR(networkOrIP)
+    if err != nil {
+        log.Fatal(err)
+    }
+    mmdbRecord, err := db.City(network)
+    if err != nil {
+        log.Fatal(err)
+    }
+    if !(strings.EqualFold(correction[1], mmdbRecord.Country.IsoCode)) ||
+       !(strings.EqualFold(correction[2], mmdbRecord.City.Names["en"])) {
+        firstSubdivision := ""
+        for i, sd := range mmdbRecord.Subdivisions {
+            if i == 0 {
+                firstSubdivision = sd.IsoCode
+                break
+            }
+        }
+        fmt.Printf(
+            "Found a potential improvement: %v, current country: '%v',suggested country: '%v', current city: '%v', suggested city: '%v', current region: '%v', suggested region: '%v,'\n",
+            networkOrIP,
+            mmdbRecord.Country.IsoCode,
+            correction[1],
+            mmdbRecord.City.Names["en"],
+            correction[3],
+            firstSubdivision,
+            correction[2],
+        )
+        return 1
+    }
+    return 0
+}
+
+func parseArgs() (string, string, error) {
+    geofeedPath := flag.String(
+        "geofeed-path",
+        "",
+        "Path to the local geofeed file to verify",
+    )
+
+    mmdbPath := flag.String(
+        "mmdb-path",
+        "/usr/local/share/GeoIP/GeoIP2-City.mmdb",
+        "Path to MMDB file to compare geofeed file against",
+    )
+    flag.Parse()
+
+    cleanGeofeedPath := filepath.Clean(*geofeedPath)
+    cleanMMDBPath := filepath.Clean(*mmdbPath)
+
+
+    var err error
+    if ( cleanGeofeedPath == "." ) { // result of empty string, probably no arg given
+        //err = errors.New("'--geofeed-path' is required, and should be a path to a local geofeed file")
+        err = fmt.Errorf("'--geofeed-path' is required")
+    }
+    return cleanGeofeedPath, cleanMMDBPath, err
+
 }
