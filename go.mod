7f8fffab405b3e90d9da9421496defb2349d3ba2
Gregory Oschwald <goschwald@maxmind.com>
2020-10-05 13:42:03 -0700

    Fix various linting issues

--- go.mod
+++ go.mod
@@ -4,7 +4,5 @@ go 1.12
 
 require (
 	github.com/oschwald/geoip2-golang v1.4.0
-	github.com/oschwald/maxminddb-golang v1.5.0 // indirect
 	github.com/stretchr/testify v1.4.0 // indirect
-	golang.org/x/sys v0.0.0-20190919044723-0c1ff786ef13 // indirect
 )
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
-// Users will give us bulk correction files following https://tools.ietf.org/html/draft-google-self-published-geofeeds-02
+// Users will give us bulk correction files following
+// https://tools.ietf.org/html/draft-google-self-published-geofeeds-02
 // It isn't uncommon for the corrections they list to either match what we already have
 // or to be worse. This script can help us work that out. Right now, it only looks
 // at the ISO country code, but checking more fields should be easy enough.
@@ -21,9 +22,16 @@ import (
 // Usage Example:
 // go run go/check_csv_corrections/main.go /path/to/corrections.csv /path/to/mmdbfile.mmdb
 func main() {
-	geofeedFilename, mmdbFilename, err := parseArgs()
+	err := run()
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func run() error {
+	geofeedFilename, mmdbFilename, err := parseArgs()
+	if err != nil {
+		return err
 	}
 
 	db, err := geoip2.Open(mmdbFilename)
@@ -39,7 +47,7 @@ func main() {
 	totalCount, correctionCount := 0, 0
 	geofeedFH, err := os.Open(geofeedFilename) //nolint: gosec // linter doesn't realize we are cleaning the filepath
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	csvReader := csv.NewReader(geofeedFH)
 	csvReader.ReuseRecord = true
@@ -58,25 +66,26 @@ func main() {
 			break
 		}
 		if err != nil {
-			geofeedFH.Close() //nolint: gosec, errcheck
-			log.Fatal(err)
+			geofeedFH.Close() //nolint: gosec
+			return err
 		}
 		totalCount++
 		currentCorrectionCount, err := verifyCorrection(row, db)
 		if err != nil {
-			geofeedFH.Close() //nolint: gosec, errcheck
-			log.Fatal(err)
+			geofeedFH.Close() //nolint: gosec
+			return err
 		}
 		correctionCount += currentCorrectionCount
 	}
 	if err != nil && err != io.EOF {
-		log.Fatalf("Failed to read file: %s", err)
+		return fmt.Errorf("failed to read file: %s", err)
 	}
 	fmt.Printf(
 		"\nOut of %d potential corrections, %d may be different than our current mappings\n",
 		totalCount,
 		correctionCount,
 	)
+	return nil
 }
 
 func verifyCorrection(correction []string, db *geoip2.Reader) (int, error) {
@@ -116,7 +125,8 @@ func verifyCorrection(correction []string, db *geoip2.Reader) (int, error) {
 		!(strings.EqualFold(correction[2], firstSubdivision)) ||
 		!(strings.EqualFold(correction[3], mmdbRecord.City.Names["en"])) {
 		fmt.Printf(
-			"Found a potential improvement: %s, current country: '%s',suggested country: '%s', current city: '%s', suggested city: '%s', current region: '%s', suggested region: '%s'\n",
+			"Found a potential improvement: %s, current country: '%s',suggested country: '%s', "+
+				"current city: '%s', suggested city: '%s', current region: '%s', suggested region: '%s'\n",
 			networkOrIP,
 			mmdbRecord.Country.IsoCode,
 			correction[1],
@@ -149,9 +159,7 @@ func parseArgs() (string, string, error) {
 
 	var err error
 	if cleanGeofeedPath == "." { // result of empty string, probably no arg given
-		//err = errors.New("'--geofeed-path' is required, and should be a path to a local geofeed file")
 		err = fmt.Errorf("'--geofeed-path' is required")
 	}
 	return cleanGeofeedPath, cleanMMDBPath, err
-
 }
