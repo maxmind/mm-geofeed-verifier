7ca390c393cc2c7bb622736e646ea9bc01a2f36b
mm-david <130688551+mm-david@users.noreply.github.com>
2023-04-14 15:19:32 -0400

    Rename lastSubdivision to mostSpecificSubdivision

--- verify/main.go
+++ verify/main.go
@@ -135,14 +135,14 @@ func verifyCorrection(correction []string, db, ispdb *geoip2.Reader, asnCounts m
 		return "", fmt.Errorf("unable to find city record for %s: %w", networkOrIP, err)
 	}
 
-	lastSubdivision := ""
+	mostSpecificSubdivision := ""
 	if len(mmdbRecord.Subdivisions) > 0 {
-		lastSubdivision = mmdbRecord.Subdivisions[len(mmdbRecord.Subdivisions)-1].IsoCode
+		mostSpecificSubdivision = mmdbRecord.Subdivisions[len(mmdbRecord.Subdivisions)-1].IsoCode
 	}
 	// ISO-3166-2 region codes are prefixed with the ISO country code,
 	// but we accept just the region code part
 	if strings.Contains(correction[2], "-") {
-		lastSubdivision = mmdbRecord.Country.IsoCode + "-" + lastSubdivision
+		mostSpecificSubdivision = mmdbRecord.Country.IsoCode + "-" + mostSpecificSubdivision
 	}
 
 	asNumber := uint(0)
@@ -179,13 +179,13 @@ func verifyCorrection(correction []string, db, ispdb *geoip2.Reader, asnCounts m
 		)
 	}
 
-	if !(strings.EqualFold(correction[2], lastSubdivision)) {
+	if !(strings.EqualFold(correction[2], mostSpecificSubdivision)) {
 		foundDiff = true
 		lines = append(
 			lines,
 			fmt.Sprintf(
 				"current region: '%s'%ssuggested region: '%s'",
-				lastSubdivision,
+				mostSpecificSubdivision,
 				indent,
 				correction[2],
 			),
